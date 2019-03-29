5bddac14d4d55b84fc76424fa28af74d3606e65b
SengMing <nitricrage@gmail.com>
2019-03-29 00:34:01 -0400

    Added sandeep's xor version but with only chosen bits for guess

--- matlab/test_guessed_hamming_distances.m
+++ matlab/test_guessed_hamming_distances.m
@@ -3,3 +3,5 @@ pkg load image
 pkg load communications
 clc;
 hamming = get_guess_lr_hamming_distance();
+
+save guess.mat hamming
--- matlab/test_hamming_distances.m
+++ matlab/test_hamming_distances.m
@@ -1,31 +1,38 @@
 %% CPA for round 16
 
-load Round16
+%load Round16
 
 hamming = get_lr_hamming_distance();
+%size(hamming)
+%local_maxima = max(PT16round)';
+%size(local_maxima)
+%
+%hd_average_map = zeros(32, 2);
+%
+%hamming_power_mapping = [hamming local_maxima];
+%
+%size(hamming_power_mapping)
+%for i = 1:32
+%  hd_average_map(i, 1) = i;
+%  bucket_count = 0;
+%  for j = 1:1000
+%    if hamming_power_mapping(j, 1) == i
+%        hd_average_map(i, 2) = hd_average_map(i, 2) + hamming_power_mapping(j, 2);
+%        bucket_count = bucket_count + 1;
+%    end
+%  end
+%  hd_average_map(i, 2) = hd_average_map(i,2)/bucket_count;
+%end
+%
+%  hd_average_map
+%  size(hd_average_map)
+%
+%plot(hd_average_map(:,1), hd_average_map(:,2))
+%
+load MAs
+P = PT';
 size(hamming)
-local_maxima = max(PT16round)';
-size(local_maxima)
-
-hd_average_map = zeros(32, 2);
-
-hamming_power_mapping = [hamming local_maxima];
-
-size(hamming_power_mapping)
-for i = 1:32
-  hd_average_map(i, 1) = i;
-  bucket_count = 0;
-  for j = 1:1000
-    if hamming_power_mapping(j, 1) == i
-        hd_average_map(i, 2) = hd_average_map(i, 2) + hamming_power_mapping(j, 2);
-        bucket_count = bucket_count + 1;
-    end
-  end
-  hd_average_map(i, 2) = hd_average_map(i,2)/bucket_count;
-end
-
-  hd_average_map
-  size(hd_average_map)
-
-plot(hd_average_map(:,1), hd_average_map(:,2))
+size(P(1:1000, :))
+Gcorr = corr(hamming,P(1:1000,:));
+figure;plot(Gcorr);title('correct guess');xlabel('sample point');ylabel('correlation');
 pause;
