b9fcc22da86aaab83e9e936e72059212353a0223
Sengming <nitricrage@gmail.com>
2019-04-03 23:08:33 -0400

    Sbox guesses for all 8 sboxes
    Pardon the manual way of doing it in main.m but I wanted more control over the way we process individual sbox guess results.

--- matlab/main.m
+++ matlab/main.m
@@ -65,17 +65,82 @@ disp('Getting key for 1st SBOX in round 16 ...')
 
 [result, L15s] = get_guess_lr_hamming_distance2(C,M);
 
-P = PT(Ind_sample-5:Ind_sample+5,:)';
-HD = result;
+P = PT(Ind_sample,:)';
+HD = result(:,:,1);
 
 
-Gcorr_sol=corr(HD,P);
-figure; plot(sum(Gcorr_sol,2)./10)
+Gcorr_sol1=corr(HD,P);
+figure; plot(sum(Gcorr_sol1,2))
 savefig('./results/Corr_SBOX1.fig')
 saveas(gcf,'./results/Corr_SBOX1.pdf')
 
+disp('Getting key for 2nd SBOX in round 16 ...')
+
+P = PT(Ind_sample,:)';
+HD = result(:,:,2);
+
+Gcorr_sol2=corr(HD,P);
+figure; plot(sum(Gcorr_sol2,2))
+savefig('./results/Corr_SBOX2.fig')
+saveas(gcf,'./results/Corr_SBOX2.pdf')
+
+disp('Getting key for 3rd SBOX in round 16 ...')
+
+P = PT(Ind_sample,:)';
+HD = result(:,:,3);
+
+Gcorr_sol3=corr(HD,P);
+figure; plot(sum(Gcorr_sol3,2))
+savefig('./results/Corr_SBOX3.fig')
+saveas(gcf,'./results/Corr_SBOX3.pdf')
+
+disp('Getting key for 4th SBOX in round 16 ...')
+
+P = PT(Ind_sample,:)';
+HD = result(:,:,4);
+
+Gcorr_sol4=corr(HD,P);
+figure; plot(sum(Gcorr_sol4,2))
+savefig('./results/Corr_SBOX4.fig')
+saveas(gcf,'./results/Corr_SBOX4.pdf')
+
+disp('Getting key for 5th SBOX in round 16 ...')
+
+P = PT(Ind_sample,:)';
+HD = result(:,:,5);
+
+Gcorr_sol5=corr(HD,P);
+figure; plot(sum(Gcorr_sol5,2))
+savefig('./results/Corr_SBOX5.fig')
+saveas(gcf,'./results/Corr_SBOX5.pdf')
+
+disp('Getting key for 6th SBOX in round 16 ...')
+
+P = PT(Ind_sample,:)';
+HD = result(:,:,6);
+
+Gcorr_sol6=corr(HD,P);
+figure; plot(sum(Gcorr_sol6,2))
+savefig('./results/Corr_SBOX6.fig')
+saveas(gcf,'./results/Corr_SBOX6.pdf')
 
 
+disp('Getting key for 7th SBOX in round 16 ...')
 
+P = PT(Ind_sample,:)';
+HD = result(:,:,7);
 
+Gcorr_sol7=corr(HD,P);
+figure; plot(sum(Gcorr_sol7,2))
+savefig('./results/Corr_SBOX7.fig')
+saveas(gcf,'./results/Corr_SBOX7.pdf')
 
+disp('Getting key for 8th SBOX in round 16 ...')
+
+P = PT(Ind_sample,:)';
+HD = result(:,:,8);
+
+Gcorr_sol8=corr(HD,P);
+figure; plot(sum(Gcorr_sol8,2))
+savefig('./results/Corr_SBOX8.fig')
+saveas(gcf,'./results/Corr_SBOX8.pdf')
